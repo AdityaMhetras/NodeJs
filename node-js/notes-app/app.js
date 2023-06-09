6e41f10869e2fec18d520c5d707447e71b8b665d
Aditya <mhetrasaditya@gmail.com>
2023-06-09 23:09:58 +0530

    yargs tutorial

--- node-js/notes-app/app.js
+++ node-js/notes-app/app.js
@@ -1,8 +1,46 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-console.log(getNotes());
-console.log(chalk.blue.bold.inverse('SUCCESS!'));
+// customize yargs version
+yargs.version('1.1.0')
 
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note');
+    }
+})
 
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'REmove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    handler: function () {
+        console.log('Reading the note');
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'listing the notes',
+    handler: function () {
+        console.log('listing the notes');
+    }
+})
+
+// add, remove, read, list
+console.log(yargs.argv)
 
