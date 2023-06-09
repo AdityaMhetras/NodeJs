61a9564ae91259dbc8b973206852128efd9ae53d
Aditya <mhetrasaditya@gmail.com>
2023-06-09 23:17:22 +0530

    added builder to a command

--- node-js/notes-app/app.js
+++ node-js/notes-app/app.js
@@ -9,6 +9,11 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title'
+        }
+    },
     handler: function () {
         console.log('Adding a new note');
     }
