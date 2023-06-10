2741c9e94e0c760ed2e64749193f55665eddbed5
Aditya <mhetrasaditya@gmail.com>
2023-06-10 18:18:16 +0530

    read from, modify and write into JSON

--- node-js/playground/1-json.js
+++ node-js/playground/1-json.js
@@ -8,7 +8,23 @@ const fs = require('fs')
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('1-json.json', bookJSON)
 
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+//load json file
 const dataBuffer = fs.readFileSync('1-json.json')
+console.log('dataBuffer'+dataBuffer);
+// convert file into JSON
 const dataJSON = dataBuffer.toString()
+console.log(dataJSON);
+// convert JSON into JS object
 const data = JSON.parse(dataJSON)
-console.log(data.title)
+console.log(data)
+console.log(data.name)
+data.name = 'Aditya'
+data.age = 31
+
+const dataJSONNew = JSON.stringify(data)
+fs.writeFileSync('1-json.json',dataJSONNew)
