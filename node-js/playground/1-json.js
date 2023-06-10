eb542fbd555a54334a2959ce2b3cda3287a68c5f
Aditya <mhetrasaditya@gmail.com>
2023-06-10 16:44:42 +0530

    read from json

--- node-js/playground/1-json.js
+++ node-js/playground/1-json.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday'
-}
-// convert object to JSON
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json', bookJSON)
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday'
+// }
+// // convert object to JSON
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
 
+const dataBuffer = fs.readFileSync('1-json.json')
+console.log(dataBuffer.toString());
